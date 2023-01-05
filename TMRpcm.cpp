dd3f4294999725c5e2adb1e9e09b23a887f7f6d7
Adam Kehl <adambriankehl@gmail.com>
2023-01-04 17:23:19 -0800

    Clang Format and Change Buffer Size

--- TMRpcm.cpp
+++ TMRpcm.cpp
@@ -3,16 +3,16 @@
 #include <pcmConfig.h>
 
 const byte togByte = _BV(ICIE1); //Get the value for toggling the buffer interrupt on/off
-#define buffSize 504
+#define buffSize 60 // size of the audio buffer in bytes
 #define rampMega
 
-volatile byte *TIMSK[] = {&TIMSK1,&TIMSK3,&TIMSK4,&TIMSK5};
-volatile byte *TCCRnA[] = {&TCCR1A,&TCCR3A,&TCCR4A,&TCCR5A};
-volatile byte *TCCRnB[] = {&TCCR1B, &TCCR3B,&TCCR4B,&TCCR5B};
-volatile unsigned int *OCRnA[] = {&OCR1A,&OCR3A,&OCR4A,&OCR5A};
-volatile unsigned int *ICRn[] = {&ICR1, &ICR3,&ICR4,&ICR5};
-volatile unsigned int *TCNT[] = {&TCNT1,&TCNT3,&TCNT4,&TCNT5};
-volatile unsigned int *OCRnB[] = {&OCR1B, &OCR3B,&OCR4B,&OCR5B};
+volatile byte* TIMSK[] = {&TIMSK1, &TIMSK3, &TIMSK4, &TIMSK5};
+volatile byte* TCCRnA[] = {&TCCR1A, &TCCR3A, &TCCR4A, &TCCR5A};
+volatile byte* TCCRnB[] = {&TCCR1B, &TCCR3B, &TCCR4B, &TCCR5B};
+volatile unsigned int* OCRnA[] = {&OCR1A, &OCR3A, &OCR4A, &OCR5A};
+volatile unsigned int* ICRn[] = {&ICR1, &ICR3, &ICR4, &ICR5};
+volatile unsigned int* TCNT[] = {&TCNT1, &TCNT3, &TCNT4, &TCNT5};
+volatile unsigned int* OCRnB[] = {&OCR1B, &OCR3B, &OCR4B, &OCR5B};
 
 ISR(TIMER3_OVF_vect, ISR_ALIASOF(TIMER1_OVF_vect));
 ISR(TIMER3_CAPT_vect, ISR_ALIASOF(TIMER1_CAPT_vect)); // switch to manually buffering
@@ -25,16 +25,16 @@ ISR(TIMER5_CAPT_vect, ISR_ALIASOF(TIMER1_CAPT_vect)); // switch to manually buff
 
 volatile unsigned int dataEnd;
 unsigned int resolution;
-volatile boolean buffEmpty[2] = {false,false}, whichBuff = false, playing = 0;
-volatile boolean loadCounter=0;
-boolean paused = 0, rampUp = 1, _2bytes=0;
-char volMod=0;
+volatile boolean buffEmpty[2] = {false, false}, whichBuff = false, playing = 0;
+volatile boolean loadCounter = 0;
+boolean paused = 0, rampUp = 1, _2bytes = 0;
+char volMod = 0;
 volatile byte buffer[2][buffSize];
 uint16_t buffCount = 0;
 byte tt;
 SdFile sFile;
 
-void TMRpcm::timerSt(){
+void TMRpcm::timerSt() {
 	*ICRn[tt] = resolution;
 	*TCCRnA[tt] = _BV(WGM11) | _BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
 	*TCCRnB[tt] = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
@@ -42,11 +42,11 @@ void TMRpcm::timerSt(){
 
 void TMRpcm::setPin() {
 	disable();
-	pinMode(speakerPin,OUTPUT);
+	pinMode(speakerPin, OUTPUT);
 	tt = 2;
 
 	SPSR |= (1 << SPI2X);
-    SPCR &= ~((1 <<SPR1) | (1 << SPR0));
+	SPCR &= ~((1 << SPR1) | (1 << SPR0));
 }
 
 boolean TMRpcm::wavInfo(char* filename) {
@@ -57,35 +57,35 @@ boolean TMRpcm::wavInfo(char* filename) {
 	}
 
 	seek(8);
-	char wavStr[] = {'W','A','V','E'};
+	char wavStr[] = {'W', 'A', 'V', 'E'};
 	for (uint8_t i = 0; i < 4; i++) {
-		if(sFile.read() != wavStr[i]){
+		if (sFile.read() != wavStr[i]) {
 			break;
 		}
-    }
+	}
 	seek(24);
 
-    SAMPLE_RATE = sFile.read();
-    SAMPLE_RATE = sFile.read() << 8 | SAMPLE_RATE;
+	SAMPLE_RATE = sFile.read();
+	SAMPLE_RATE = sFile.read() << 8 | SAMPLE_RATE;
 
 	seek(36);
-    char datStr[4] = {'d','a','t','a'};
-    for (uint8_t i = 0; i < 4; i++) {
-		if(sFile.read() != datStr[i]) {
-		    seek(40);
-		    unsigned int siz = sFile.read();
-		    siz = (sFile.read() << 8 | siz) + 2;
-		    seek(fPosition() + siz);
-            for (byte j = 0; j < 4; j++) {
+	char datStr[4] = {'d', 'a', 't', 'a'};
+	for (uint8_t i = 0; i < 4; i++) {
+		if (sFile.read() != datStr[i]) {
+			seek(40);
+			unsigned int siz = sFile.read();
+			siz = (sFile.read() << 8 | siz) + 2;
+			seek(fPosition() + siz);
+			for (byte j = 0; j < 4; j++) {
 				if (sFile.read() != datStr[j]) {
 					return false;
 				}
-	        }
+			}
 		}
-	 }
+	}
 
 	unsigned long dataBytes = sFile.read();
-    for (byte i = 8; i < 32; i += 8) {
+	for (byte i = 8; i < 32; i += 8) {
 		dataBytes = sFile.read() << i | dataBytes;
 	}
 
@@ -95,86 +95,86 @@ boolean TMRpcm::wavInfo(char* filename) {
 }
 
 void TMRpcm::quality(boolean q) {
-//	if (!playing) {
-//		qual = q;
-//	}
+	//	if (!playing) {
+	//		qual = q;
+	//	}
 }
 
 void TMRpcm::stopPlayback() {
-//	playing = 0;
-//	*TIMSK[tt] &= ~(togByte | _BV(TOIE1));
-//	if (ifOpen()) {
-//		sFile.close();
-//	}
+	playing = false;
+	*TIMSK[tt] &= ~(togByte | _BV(TOIE1));
+	if (ifOpen()) {
+		sFile.close();
+	}
 }
 
 void TMRpcm::pause() {
-//	paused = !paused;
-//	if (!paused && playing) {
-//		*TIMSK[tt] |= ( _BV(ICIE1) | _BV(TOIE1) );
-//	} else if (paused && playing) {
-//		*TIMSK[tt] &= ~( _BV(TOIE1) );
-//	}
+	//	paused = !paused;
+	//	if (!paused && playing) {
+	//		*TIMSK[tt] |= ( _BV(ICIE1) | _BV(TOIE1) );
+	//	} else if (paused && playing) {
+	//		*TIMSK[tt] &= ~( _BV(TOIE1) );
+	//	}
 }
 
-boolean TMRpcm::seek(unsigned long pos ){
+boolean TMRpcm::seek(unsigned long pos) {
 	return sFile.seekSet(pos);
 }
 
-unsigned long TMRpcm::fPosition(){
+unsigned long TMRpcm::fPosition() {
 	return sFile.curPosition();
 }
 
-boolean TMRpcm::ifOpen(){
+boolean TMRpcm::ifOpen() {
 	return sFile.isOpen();
 }
 
-#if !defined (ENABLE_MULTI) //Below section for normal playback of 1 track at a time
+#if !defined(ENABLE_MULTI) //Below section for normal playback of 1 track at a time
 
 
-void TMRpcm::play(char* filename){
-	play(filename,0);
+void TMRpcm::play(char* filename) {
+	play(filename, 0);
 }
 
 
 void TMRpcm::play(char* filename, unsigned long seekPoint) {
 	if (speakerPin != lastSpeakPin) {
 		setPin();
-		lastSpeakPin=speakerPin;
+		lastSpeakPin = speakerPin;
 	}
 	stopPlayback();
 
 	// verify it's a valid wav file, also open the file to SdFile
 	if (!wavInfo(filename)) {
 		Serial.println("[ERROR] invalid wav file");
-        return;
-    }
+		return;
+	}
 
 	// skip the header info
-  	if (seekPoint > 0) {
+	if (seekPoint > 0) {
 		seekPoint = (SAMPLE_RATE * seekPoint) + fPosition();
-  		seek(seekPoint);
+		seek(seekPoint);
 	}
 
 	playing = true;
 	paused = false;
 
 	// cap max sample rate
-	if (SAMPLE_RATE > 45050 ) {
+	if (SAMPLE_RATE > 45050) {
 		SAMPLE_RATE = 24000;
-  	}
+	}
 
 	// quality 0 = 8 bit, quality 1 = 16 bit
 #ifdef EIGHT_BIT_AUDIO
-	resolution = 8 * (1000000/SAMPLE_RATE);
+	resolution = 8 * (1000000 / SAMPLE_RATE);
 #endif
 #ifdef SIXTEEN_BIT_AUDIO
-		resolution = 16 * (1000000/SAMPLE_RATE);
+	resolution = 16 * (1000000 / SAMPLE_RATE);
 #endif
 
-    byte tmp = (sFile.read() + sFile.peek()) / 2;
+	byte tmp = (sFile.read() + sFile.peek()) / 2;
 
-    if (rampUp) {
+	if (rampUp) {
 		*OCRnA[tt] = 0;
 		*OCRnB[tt] = resolution;
 		timerSt();
@@ -198,41 +198,41 @@ void TMRpcm::play(char* filename, unsigned long seekPoint) {
 			buffer[0][i] = mod;
 		}
 		for (unsigned int i = 0; i < buffSize; i++) {
-			mod = constrain(mod+1,mod, tmp);
+			mod = constrain(mod + 1, mod, tmp);
 			buffer[1][i] = mod;
 		}
 	} else {
 		for (unsigned int i = 0; i < buffSize; i++) {
-			mod = constrain(mod-1,tmp ,mod);
+			mod = constrain(mod - 1, tmp, mod);
 			buffer[0][i] = mod;
 		}
 		for (unsigned int i = 0; i < buffSize; i++) {
-			mod = constrain(mod-1,tmp, mod);
+			mod = constrain(mod - 1, tmp, mod);
 			buffer[1][i] = mod;
 		}
 	}
 
-    whichBuff = 0;
+	whichBuff = 0;
 	buffEmpty[0] = true;
 	buffEmpty[1] = true;
 	buffCount = 0;
 
-    noInterrupts();
+	noInterrupts();
 	timerSt();
-    *TIMSK[tt] = ( togByte | _BV(TOIE1) );
-    interrupts();
+	*TIMSK[tt] = (togByte | _BV(TOIE1));
+	interrupts();
 }
 
-void TMRpcm::volume(char upDown){
-  if(upDown){
-	  volMod++;
-  }else{
-	  volMod--;
-  }
+void TMRpcm::volume(char upDown) {
+	if (upDown) {
+		volMod++;
+	} else {
+		volMod--;
+	}
 }
 
 void TMRpcm::setVolume(char vol) {
-    volMod = vol - 4 ;
+	volMod = vol - 4;
 }
 
 // load from sd to buffers
@@ -243,10 +243,10 @@ void TMRpcm::fillBuffers() {
 		a = !whichBuff;
 		*TIMSK[tt] &= ~togByte;
 		sei();
-		sFile.read((byte*)buffer[a],buffSize);
+		sFile.read((byte*) buffer[a], buffSize);
 		if (sFile.available() <= dataEnd) {
 			playing = 0;
-			*TIMSK[tt] &= ~( togByte | _BV(TOIE1) );
+			*TIMSK[tt] &= ~(togByte | _BV(TOIE1));
 			if (sFile.isOpen()) {
 				sFile.close();
 			}
@@ -260,62 +260,62 @@ void TMRpcm::fillBuffers() {
 // switch to manually buffering
 ISR(TIMER1_CAPT_vect) {
 	//TMRpcm::fillBuffers();
-//	if (buffEmpty[!whichBuff]) {
-//		a = !whichBuff;
-//		*TIMSK[tt] &= ~togByte;
-//		sei();
-//		sFile.read((byte*)buffer[a],buffSize);
-//		if (sFile.available() <= dataEnd) {
-//		  	playing = false;
-//		  	*TIMSK[tt] &= ~( togByte | _BV(TOIE1) );
-//			  if (sFile.isOpen()) {
-//				  sFile.close();
-//			  }
-//			return;
-//	  	}
-//		buffEmpty[a] = 0;
-//		*TIMSK[tt] |= togByte;
-//   	}
+	//	if (buffEmpty[!whichBuff]) {
+	//		a = !whichBuff;
+	//		*TIMSK[tt] &= ~togByte;
+	//		sei();
+	//		sFile.read((byte*)buffer[a],buffSize);
+	//		if (sFile.available() <= dataEnd) {
+	//		  	playing = false;
+	//		  	*TIMSK[tt] &= ~( togByte | _BV(TOIE1) );
+	//			  if (sFile.isOpen()) {
+	//				  sFile.close();
+	//			  }
+	//			return;
+	//	  	}
+	//		buffEmpty[a] = 0;
+	//		*TIMSK[tt] |= togByte;
+	//   	}
 }
 
 ISR(TIMER1_OVF_vect) {
-//	if (volMod < 0 ) {
-//		*OCRnA[tt] = *OCRnB[tt] = buffer[whichBuff][buffCount] >> (volMod*-1);
-//    } else {
-//		*OCRnA[tt] = *OCRnB[tt] = buffer[whichBuff][buffCount] << volMod;
-//    }
+	//	if (volMod < 0 ) {
+	//		*OCRnA[tt] = *OCRnB[tt] = buffer[whichBuff][buffCount] >> (volMod*-1);
+	//    } else {
+	//		*OCRnA[tt] = *OCRnB[tt] = buffer[whichBuff][buffCount] << volMod;
+	//    }
 	*OCRnA[tt] = *OCRnB[tt] = buffer[whichBuff][buffCount];
-    buffCount++;
+	buffCount++;
 
 	// check if a buffer is empty, then move to the other buffer
-  	if (buffCount >= buffSize) {
-	  buffCount = 0;
-      buffEmpty[whichBuff] = true;
-      whichBuff = !whichBuff;
-  	}
+	if (buffCount >= buffSize) {
+		buffCount = 0;
+		buffEmpty[whichBuff] = true;
+		whichBuff = !whichBuff;
+	}
 }
 
 void TMRpcm::disable() {
-//	playing = 0;
-//	*TIMSK[tt] &= ~( togByte | _BV(TOIE1) );
-//	if (ifOpen()) {
-//		sFile.close();
-//	}
-//	if (bitRead(*TCCRnA[tt],7) > 0) {
-//		int current = *OCRnA[tt];
-//		for (int i = 0; i < resolution; i++) {
-//			*OCRnB[tt] = constrain((current + i),0,resolution);
-//			*OCRnA[tt] = constrain((current - i),0,resolution);
-//			for(int j = 0; j < 10; j++){
-//				while(*TCNT[tt] < resolution-50){}
-//			}
-//		}
-//	}
-//    rampUp = 1;
-//    *TCCRnA[tt] = *TCCRnB[tt] = 0;
+	//	playing = 0;
+	//	*TIMSK[tt] &= ~( togByte | _BV(TOIE1) );
+	//	if (ifOpen()) {
+	//		sFile.close();
+	//	}
+	//	if (bitRead(*TCCRnA[tt],7) > 0) {
+	//		int current = *OCRnA[tt];
+	//		for (int i = 0; i < resolution; i++) {
+	//			*OCRnB[tt] = constrain((current + i),0,resolution);
+	//			*OCRnA[tt] = constrain((current - i),0,resolution);
+	//			for(int j = 0; j < 10; j++){
+	//				while(*TCNT[tt] < resolution-50){}
+	//			}
+	//		}
+	//	}
+	//    rampUp = 1;
+	//    *TCCRnA[tt] = *TCCRnB[tt] = 0;
 }
 
-boolean TMRpcm::isPlaying(){
+boolean TMRpcm::isPlaying() {
 	return playing;
 }
 
@@ -323,8 +323,8 @@ boolean TMRpcm::isPlaying(){
 
 byte TMRpcm::getInfo(char* filename, char* tagData, byte infoNum) {
 	byte gotInfo = 0;
-	if ((gotInfo = metaInfo(1,filename, tagData, infoNum)) < 1) {
-		gotInfo = metaInfo(0,filename, tagData, infoNum);
+	if ((gotInfo = metaInfo(1, filename, tagData, infoNum)) < 1) {
+		gotInfo = metaInfo(0, filename, tagData, infoNum);
 	}
 	return gotInfo;
 }
@@ -334,35 +334,36 @@ byte TMRpcm::listInfo(char* filename, char* tagData, byte infoNum) {
 }
 
 //http://id3.org/id3v2.3.0
-byte TMRpcm::id3Info(char* filename, char* tagData, byte infoNum){
+byte TMRpcm::id3Info(char* filename, char* tagData, byte infoNum) {
 	return metaInfo(1, filename, tagData, infoNum);
 }
 
-unsigned long TMRpcm::searchMainTags(SdFile xFile, char *datStr){
+unsigned long TMRpcm::searchMainTags(SdFile xFile, char* datStr) {
 	xFile.seekSet(36);
 	boolean found = 0;
-    char dChars[4] = {'d','a','t','a'};
+	char dChars[4] = {'d', 'a', 't', 'a'};
 	char tmpChars[4];
 
 	//xFile.seek(36);
-    xFile.read((char*)tmpChars,4);
-    for (byte i =0; i<4; i++){
-		if(tmpChars[i] != dChars[i]){
+	xFile.read((char*) tmpChars, 4);
+	for (byte i = 0; i < 4; i++) {
+		if (tmpChars[i] != dChars[i]) {
 			xFile.seekSet(40);
-			unsigned int siz = xFile.read(); siz = (xFile.read() << 8 | siz)+2;
-		    xFile.seekSet(xFile.curPosition() + siz);
-		    xFile.read((char*)tmpChars,4);
-            for (byte i =0; i<4; i++){
-				if(tmpChars[i] != dChars[i]){
+			unsigned int siz = xFile.read();
+			siz = (xFile.read() << 8 | siz) + 2;
+			xFile.seekSet(xFile.curPosition() + siz);
+			xFile.read((char*) tmpChars, 4);
+			for (byte i = 0; i < 4; i++) {
+				if (tmpChars[i] != dChars[i]) {
 					return 0;
 				}
-	        }
+			}
 		}
-	 }
+	}
 
-	unsigned long tmpp=0;
+	unsigned long tmpp = 0;
 	unsigned long daBytes = xFile.read();
-    for (byte i =8; i<32; i+=8){
+	for (byte i = 8; i < 32; i += 8) {
 		tmpp = xFile.read();
 		daBytes = tmpp << i | daBytes;
 	}
@@ -374,12 +375,12 @@ unsigned long TMRpcm::searchMainTags(SdFile xFile, char *datStr){
 	xFile.seekSet(daBytes);
 	while (xFile.available() > 5) {
 		if (xFile.read() == datStr[0] && xFile.peek() == datStr[1]) {
-			xFile.read((char*)tmpChars,3);
-            if (tmpChars[1] == datStr[2] && tmpChars[2] == datStr[3]) {
+			xFile.read((char*) tmpChars, 3);
+			if (tmpChars[1] == datStr[2] && tmpChars[2] == datStr[3]) {
 				found = 1;
 				return xFile.curPosition();
 			} else {
-				unsigned long pos = xFile.curPosition()-1;
+				unsigned long pos = xFile.curPosition() - 1;
 				xFile.seekSet(pos - 4);
 			}
 		}
@@ -388,131 +389,157 @@ unsigned long TMRpcm::searchMainTags(SdFile xFile, char *datStr){
 }
 
 byte TMRpcm::metaInfo(boolean infoType, char* filename, char* tagData, byte whichInfo) {
-	if (ifOpen()) { noInterrupts();}
+	if (ifOpen()) {
+		noInterrupts();
+	}
 
-		SdFile xFile;
-		xFile.open(filename);
-		xFile.seekSet(36);
+	SdFile xFile;
+	xFile.open(filename);
+	xFile.seekSet(36);
 
-	boolean found=0;
-		char* datStr = "LIST";
-		if(infoType == 1){datStr = "ID3 "; datStr[3] = 3;}
-		char tmpChars[4];
+	boolean found = 0;
+	char* datStr = "LIST";
+	if (infoType == 1) {
+		datStr = "ID3 ";
+		datStr[3] = 3;
+	}
+	char tmpChars[4];
 
-	if(infoType == 0){ //if requesting LIST info, check for data at beginning of file first
-		xFile.read((char*)tmpChars,4);
-		for (byte i=0; i<4; i++){ //4 tagSize
-			if(tmpChars[i] != datStr[i]){
+	if (infoType == 0) { //if requesting LIST info, check for data at beginning of file first
+		xFile.read((char*) tmpChars, 4);
+		for (byte i = 0; i < 4; i++) { //4 tagSize
+			if (tmpChars[i] != datStr[i]) {
 				break;
-		  	}else if(i==3){
+			} else if (i == 3) {
 				found = 1;
-		  	}
+			}
 		}
 	}
-	if(found == 0){
+	if (found == 0) {
 
-			unsigned long pos = searchMainTags(xFile, datStr);
-			xFile.seekSet(pos);
-			if(pos > 0){ found = 1; }
-
+		unsigned long pos = searchMainTags(xFile, datStr);
+		xFile.seekSet(pos);
+		if (pos > 0) {
+			found = 1;
+		}
 	}
 
-//** This section finds the starting point and length of the tag info
-	if(found == 0){ xFile.close(); if(ifOpen()){ interrupts();} return 0; }
+	//** This section finds the starting point and length of the tag info
+	if (found == 0) {
+		xFile.close();
+		if (ifOpen()) {
+			interrupts();
+		}
+		return 0;
+	}
 
 	unsigned long listEnd;
 	unsigned int listLen;
-	char* tagNames[] = {"INAM","IART","IPRD"};
+	char* tagNames[] = {"INAM", "IART", "IPRD"};
 
-	if(infoType == 0){ //LIST format
-		listLen = xFile.read(); listLen = xFile.read() << 8 | listLen;
+	if (infoType == 0) { //LIST format
+		listLen = xFile.read();
+		listLen = xFile.read() << 8 | listLen;
 
-			xFile.seekSet(xFile.curPosition() +6);
-			listEnd = xFile.curPosition() + listLen;
+		xFile.seekSet(xFile.curPosition() + 6);
+		listEnd = xFile.curPosition() + listLen;
 
 
-	}else{				//ID3 format
+	} else { //ID3 format
 
-			xFile.seekSet(xFile.curPosition() + 5);
+		xFile.seekSet(xFile.curPosition() + 5);
 
-			listLen = xFile.read() << 7 | listLen; listLen = xFile.read() | listLen;
-			tagNames[0] = "TPE1"; tagNames[1] ="TIT2"; tagNames[2] ="TALB";
+		listLen = xFile.read() << 7 | listLen;
+		listLen = xFile.read() | listLen;
+		tagNames[0] = "TPE1";
+		tagNames[1] = "TIT2";
+		tagNames[2] = "TALB";
 
-			listEnd = xFile.curPosition() + listLen;
+		listEnd = xFile.curPosition() + listLen;
 	}
 
 	char tgs[4];
 	unsigned int len = 0;
 	unsigned long tagPos = 0;
 
-//** This section reads the tags and gets the size of the tag data and its position
-//** Should work with very long tags if a big enough buffer is provided
+	//** This section reads the tags and gets the size of the tag data and its position
+	//** Should work with very long tags if a big enough buffer is provided
 
-	while(xFile.curPosition() < listEnd){
+	while (xFile.curPosition() < listEnd) {
 
 
-		xFile.read((char*)tgs,4);
+		xFile.read((char*) tgs, 4);
 
-		if(infoType == 0){ //LIST
-			len = xFile.read()-1;
+		if (infoType == 0) { //LIST
+			len = xFile.read() - 1;
 			len = xFile.read() << 8 | len;
 
-				xFile.seekSet(xFile.curPosition()+2);
+			xFile.seekSet(xFile.curPosition() + 2);
 
-		}else{ 				//ID3
+		} else { //ID3
 
-				xFile.seekSet(xFile.curPosition()+3);
+			xFile.seekSet(xFile.curPosition() + 3);
 
 			len = xFile.read();
 			len = xFile.read() << 8 | len;
-			len = (len-3)/2;
+			len = (len - 3) / 2;
 
-			    xFile.seekSet(xFile.curPosition() +4);
-				tagPos = xFile.curPosition();
-
-
+			xFile.seekSet(xFile.curPosition() + 4);
+			tagPos = xFile.curPosition();
 		}
 
-		found =0;
-	//** This section checks to see if the tag we found is the one requested
-	//** If so, it loads the data into the buffer
-		for(int p=0; p<4;p++){
-			if(tgs[p] != tagNames[whichInfo][p]){
+		found = 0;
+		//** This section checks to see if the tag we found is the one requested
+		//** If so, it loads the data into the buffer
+		for (int p = 0; p < 4; p++) {
+			if (tgs[p] != tagNames[whichInfo][p]) {
 				break;
-			}else{
-				if(p==3){
-					if(infoType == 1){
+			} else {
+				if (p == 3) {
+					if (infoType == 1) {
 						byte junk;
-						for(byte j=0; j<len; j++){
+						for (byte j = 0; j < len; j++) {
 							tagData[j] = xFile.read();
-							junk=xFile.read();
+							junk = xFile.read();
 						}
-					}else{
-						xFile.read((char*)tagData,len);
+					} else {
+						xFile.read((char*) tagData, len);
 					}
 					tagData[len] = '\0';
 					xFile.close();
-					if(ifOpen()){ interrupts();}
- 					return len;
+					if (ifOpen()) {
+						interrupts();
+					}
+					return len;
 				}
 			}
 		}
 
-		if(found){break;}
-
-	//**This section jumps to the next tag position if the requested tag wasn't found
-
-		if(infoType == 0){
-			if(!found){	xFile.seekSet(xFile.curPosition()+len);}
-			while(xFile.peek() == 0){xFile.read();}
-		}else{
-			if(!found){	xFile.seekSet(tagPos+len); }
-			while(xFile.peek() != 'T'){xFile.read();}
+		if (found) {
+			break;
 		}
 
+		//**This section jumps to the next tag position if the requested tag wasn't found
 
- 	}
- 	xFile.close();
- 	if(ifOpen()){ interrupts();}
- 	return len;
+		if (infoType == 0) {
+			if (!found) {
+				xFile.seekSet(xFile.curPosition() + len);
+			}
+			while (xFile.peek() == 0) {
+				xFile.read();
+			}
+		} else {
+			if (!found) {
+				xFile.seekSet(tagPos + len);
+			}
+			while (xFile.peek() != 'T') {
+				xFile.read();
+			}
+		}
+	}
+	xFile.close();
+	if (ifOpen()) {
+		interrupts();
+	}
+	return len;
 }
--- TMRpcm.h
+++ TMRpcm.h
@@ -1,41 +1,39 @@
-#ifndef TMRpcm_h   // if x.h hasn't been included yet...
-#define TMRpcm_h   //   #define this so the compiler knows it has been included
+#ifndef TMRpcm_h // if x.h hasn't been included yet...
+#define TMRpcm_h //   #define this so the compiler knows it has been included
 
 #include <Arduino.h>
 #include <pcmConfig.h>
 #include <SdFat.h>
 
 class TMRpcm {
-public:
-	void play(char* filename);
-	void stopPlayback();
-	void volume(char vol);
-	void setVolume(char vol);
-	void disable();
-	void pause();
-	void quality(boolean q);
-	static void fillBuffers();
-	byte speakerPin;
-	boolean wavInfo(char* filename);
-	boolean isPlaying();
-	boolean rfPlaying;
-	unsigned int SAMPLE_RATE;
-	byte listInfo(char* filename, char *tagData, byte infoNum);
-	byte id3Info(char* filename, char *tagData, byte infoNum);
-	byte getInfo(char* filename, char* tagData, byte infoNum);
-	void play(char* filename, unsigned long seekPoint);
+	public:
+		void play(char* filename);
+		void stopPlayback();
+		void volume(char vol);
+		void setVolume(char vol);
+		void disable();
+		void pause();
+		void quality(boolean q);
+		static void fillBuffers();
+		byte speakerPin;
+		boolean wavInfo(char* filename);
+		boolean isPlaying();
+		boolean rfPlaying;
+		unsigned int SAMPLE_RATE;
+		byte listInfo(char* filename, char* tagData, byte infoNum);
+		byte id3Info(char* filename, char* tagData, byte infoNum);
+		byte getInfo(char* filename, char* tagData, byte infoNum);
+		void play(char* filename, unsigned long seekPoint);
 
-private:
-	byte lastSpeakPin;
-	void setPin();
-	void timerSt();
-	byte metaInfo(boolean infoType, char* filename, char* tagData, byte whichInfo);
-	boolean seek(unsigned long pos);
-	boolean ifOpen();
-	unsigned long fPosition();
-	unsigned long searchMainTags(SdFile xFile, char *datStr);
+	private:
+		byte lastSpeakPin;
+		void setPin();
+		void timerSt();
+		byte metaInfo(boolean infoType, char* filename, char* tagData, byte whichInfo);
+		boolean seek(unsigned long pos);
+		boolean ifOpen();
+		unsigned long fPosition();
+		unsigned long searchMainTags(SdFile xFile, char* datStr);
 };
 
 #endif
-
-
